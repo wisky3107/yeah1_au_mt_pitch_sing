e93c6f212f2e15e97cc199406ebd88a93e67191f
wisky3107 <wisky3107@gmail.com>
2025-04-03 21:15:27 +0700

    +ADD build stagging

--- build/stagging/application.47a87.js
+++ build/stagging/application.47a87.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.ba841.json';
+          this.settingsPath = 'src/settings.00b1f.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- build/stagging/index.843ee.js
+++ build/stagging/index.843ee.js
@@ -1,4 +1,4 @@
-System.register(["./application.42d78.js"], function (_export, _context) {
+System.register(["./application.47a87.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
