612b48305cc5d045fa2389eb07365fab52b5af45
wisky3107 <wisky3107@gmail.com>
2025-04-05 10:49:07 +0700

    +ADD new build with logic of replace timestamp

--- build/stagging/application.d79e3.js
+++ build/stagging/application.d79e3.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.2a521.json';
+          this.settingsPath = 'src/settings.125dd.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
